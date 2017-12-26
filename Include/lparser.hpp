6fd69ab2e636ad9bc9757da23e9b4e56702cfc3b
Gian Lorenzo Meocci <glmeocci@gmail.com>
2017-12-26 16:00:40 +0100

    convert tabs / space

--- Include/lparser.hpp
+++ Include/lparser.hpp
@@ -10,336 +10,336 @@
 namespace lparser
 {
 
-	template<typename T>
-	struct parser_t {
-		using value_t = T;
+    template<typename T>
+    struct parser_t {
+        using value_t = T;
 
-		parser_t() {
-			first = nullptr;
-			empty = true;
-		}
+        parser_t() {
+            first = nullptr;
+            empty = true;
+        }
 
-		parser_t(T a, std::string r = "")
-				: remain(r) {
-			first = std::make_unique<T>(std::move(a));
-			empty = false;
-		}
+        parser_t(T a, std::string r = "")
+                : remain(r) {
+            first = std::make_unique<T>(std::move(a));
+            empty = false;
+        }
 
-		parser_t(const parser_t &rhs) {
-			empty = rhs.empty;
-			if (!empty) {
-				remain = rhs.remain;
-				first = std::make_unique<T>(rhs.get());
-			} else {
-				remain = "";
-			}
-		}
+        parser_t(const parser_t &rhs) {
+            empty = rhs.empty;
+            if (!empty) {
+                remain = rhs.remain;
+                first = std::make_unique<T>(rhs.get());
+            } else {
+                remain = "";
+            }
+        }
 
-		bool is_empty() const { return empty; }
+        bool is_empty() const { return empty; }
 
-		T get() const { return *first; }
+        T get() const { return *first; }
 
-		std::unique_ptr<T> first;
-		std::string remain;
+        std::unique_ptr<T> first;
+        std::string remain;
 
-	private:
-		bool empty;
-	};
+    private:
+        bool empty;
+    };
 
-	template<typename T>
-	parser_t<T> empty() { return parser_t<T>{}; }
+    template<typename T>
+    parser_t<T> empty() { return parser_t<T>{}; }
 
-	template<typename T>
-	parser_t<T> empty_fn(std::string) { return parser_t<T>{}; }
+    template<typename T>
+    parser_t<T> empty_fn(std::string) { return parser_t<T>{}; }
 
-	template<typename Parser>
-	decltype(auto) parse(Parser p, std::string inp)
-	{
-		return p(inp);
-	}
+    template<typename Parser>
+    decltype(auto) parse(Parser p, std::string inp)
+    {
+        return p(inp);
+    }
 
-	template<typename G, typename P>
-	decltype(auto) fmap(G g, P p)
-	{
-		return [=](std::string inp) {
-			const auto a = parse(p, inp);
-			using B_t = decltype(g(a.get()));
-			if (a.is_empty())
-				return empty<B_t>();
-			else
-				return parser_t<B_t>(
-						g(a.get()),
-						a.remain
-				);
-		};
-	}
+    template<typename G, typename P>
+    decltype(auto) fmap(G g, P p)
+    {
+        return [=](std::string inp) {
+            const auto a = parse(p, inp);
+            using B_t = decltype(g(a.get()));
+            if (a.is_empty())
+                return empty<B_t>();
+            else
+                return parser_t<B_t>(
+                        g(a.get()),
+                        a.remain
+                );
+        };
+    }
 
-	template<typename T>
-	decltype(auto) pure(T v)
-	{
-		return [=](std::string inp) {
-			return parser_t<T>(
-					std::move(v),
-					inp
-			);
-		};
-	}
+    template<typename T>
+    decltype(auto) pure(T v)
+    {
+        return [=](std::string inp) {
+            return parser_t<T>(
+                    std::move(v),
+                    inp
+            );
+        };
+    }
 
-	template<typename Pg, typename Px>
-	decltype(auto) combine(Pg pg, Px px)
-	{
-		return [=](std::string inp) {
-			const auto r = parse(pg, inp);
-			using B_t = decltype(parse(fmap(r.get(), px), r.remain).get());
+    template<typename Pg, typename Px>
+    decltype(auto) combine(Pg pg, Px px)
+    {
+        return [=](std::string inp) {
+            const auto r = parse(pg, inp);
+            using B_t = decltype(parse(fmap(r.get(), px), r.remain).get());
 
-			if (r.is_empty()) {
-				return empty<B_t>();
-			} else {
-				return parse(fmap(r.get(), px), r.remain);
-			}
-		};
-	}
+            if (r.is_empty()) {
+                return empty<B_t>();
+            } else {
+                return parse(fmap(r.get(), px), r.remain);
+            }
+        };
+    }
 
-	template<typename Pg, typename Px, typename A, typename ...Args>
-	decltype(auto) combine(Pg pg, Px px, A a, Args ...args)
-	{
-		return combine(combine(pg, px), a, std::forward<Args>(args)...);
-	}
+    template<typename Pg, typename Px, typename A, typename ...Args>
+    decltype(auto) combine(Pg pg, Px px, A a, Args ...args)
+    {
+        return combine(combine(pg, px), a, std::forward<Args>(args)...);
+    }
 
-	template<typename P, typename Q>
-	decltype(auto) pipe(P p, Q q)
-	{
-		return [=](std::string inp) {
-			const auto a = parse(p, inp);
-			if (a.is_empty())
-				return parse(q, inp);
-			else
-				return a;
-		};
-	};
-
-
-	/* PARSERs */
-
-	parser_t<char> item(std::string inp)
-	{
-		if (inp.empty())
-			return empty<char>();
-		else
-		{
-			return {
-					parser_t<char>{
-							inp[0],
-							inp.substr(1)
-					}
-			};
-		}
-	}
+    template<typename P, typename Q>
+    decltype(auto) pipe(P p, Q q)
+    {
+        return [=](std::string inp) {
+            const auto a = parse(p, inp);
+            if (a.is_empty())
+                return parse(q, inp);
+            else
+                return a;
+        };
+    };
 
 
-	template<typename F>
-	decltype(auto) sat(F f)
-	{
-		return [=](std::string inp) {
-			const auto x = item(inp);
-			if (!x.is_empty() && f(x.get()))
-				return x;
-			else
-				return empty<decltype(x.get())>();
-		};
-	}
+    /* PARSERs */
 
-	decltype(auto) char_eq(char x)
-	{
-		return sat([x](char ch) { return x == ch; });
-	}
-
-	decltype(auto) digit(std::string inp)
-	{
-		return parse(sat([](char c){ return isdigit(c); }), inp);
-	}
-
-	decltype(auto) lower(std::string inp)
-	{
-		return parse(sat([](char c){ return islower(c); }), inp);
-	}
-
-	decltype(auto) upper(std::string inp)
-	{
-		return parse(sat([](char c){ return isupper(c); }), inp);
-	}
-
-	decltype(auto) letter(std::string inp)
-	{
-		return parse(sat([](char c){ return isalpha(c); }), inp);
-	}
-
-	decltype(auto) alphanum(std::string inp)
-	{
-		return parse(sat([](char c){ return isalnum(c); }), inp);
-	}
+    parser_t<char> item(std::string inp)
+    {
+        if (inp.empty())
+            return empty<char>();
+        else
+        {
+            return {
+                    parser_t<char>{
+                            inp[0],
+                            inp.substr(1)
+                    }
+            };
+        }
+    }
 
 
-	// utils for seq
-	template<typename T, typename Q>
-	std::string plus(T a, Q b)
-	{
-		std::stringstream s;
-		s << a << b;
-		return s.str();
-	}
+    template<typename F>
+    decltype(auto) sat(F f)
+    {
+        return [=](std::string inp) {
+            const auto x = item(inp);
+            if (!x.is_empty() && f(x.get()))
+                return x;
+            else
+                return empty<decltype(x.get())>();
+        };
+    }
+
+    decltype(auto) char_eq(char x)
+    {
+        return sat([x](char ch) { return x == ch; });
+    }
+
+    decltype(auto) digit(std::string inp)
+    {
+        return parse(sat([](char c){ return isdigit(c); }), inp);
+    }
+
+    decltype(auto) lower(std::string inp)
+    {
+        return parse(sat([](char c){ return islower(c); }), inp);
+    }
+
+    decltype(auto) upper(std::string inp)
+    {
+        return parse(sat([](char c){ return isupper(c); }), inp);
+    }
+
+    decltype(auto) letter(std::string inp)
+    {
+        return parse(sat([](char c){ return isalpha(c); }), inp);
+    }
+
+    decltype(auto) alphanum(std::string inp)
+    {
+        return parse(sat([](char c){ return isalnum(c); }), inp);
+    }
 
 
-	template<typename P, typename Q>
-	decltype(auto) seq(P p, Q q)
-	{
-		return [=](std::string inp) -> parser_t<std::string> {
-			const auto a = parse(p, inp);
-
-			if (a.is_empty())
-				return empty<std::string>();
-			else
-			{
-				const auto b = parse(q, a.remain);
-				if (b.is_empty())
-					return empty<std::string>();
-				else
-					return parser_t<std::string>{plus(a.get(), b.get()), b.remain};
-			}
-		};
-	}
+    // utils for seq
+    template<typename T, typename Q>
+    std::string plus(T a, Q b)
+    {
+        std::stringstream s;
+        s << a << b;
+        return s.str();
+    }
 
 
-	template<typename P, typename Q, typename ...Args>
-	decltype(auto) seq(P p, Q q, Args ...args)
-	{
-		return seq(p, seq(q, std::forward<Args>(args)...));
-	}
+    template<typename P, typename Q>
+    decltype(auto) seq(P p, Q q)
+    {
+        return [=](std::string inp) -> parser_t<std::string> {
+            const auto a = parse(p, inp);
+
+            if (a.is_empty())
+                return empty<std::string>();
+            else
+            {
+                const auto b = parse(q, a.remain);
+                if (b.is_empty())
+                    return empty<std::string>();
+                else
+                    return parser_t<std::string>{plus(a.get(), b.get()), b.remain};
+            }
+        };
+    }
 
 
-	decltype(auto) string_eq(std::string x)
-	{
-		return [=](std::string inp) {
-			const auto pos = inp.find(x);
-			if (pos == std::string::npos)
-				return empty<std::string>();
-
-			const auto l = x.size();
-			return parser_t<std::string>{
-					inp.substr(0, pos + l),
-					inp.substr(pos + l)
-			};
-		};
-	}
+    template<typename P, typename Q, typename ...Args>
+    decltype(auto) seq(P p, Q q, Args ...args)
+    {
+        return seq(p, seq(q, std::forward<Args>(args)...));
+    }
 
 
-	template<typename P>
-	decltype(auto) many(P p)
-	{
-		return [=](std::string inp) {
-			std::string c{};
+    decltype(auto) string_eq(std::string x)
+    {
+        return [=](std::string inp) {
+            const auto pos = inp.find(x);
+            if (pos == std::string::npos)
+                return empty<std::string>();
 
-			while (!inp.empty())
-			{
-				auto a = parse(p, inp);
-				if (a.is_empty())
-					break;
-
-				c += a.get();
-				inp = a.remain;
-			}
-
-			return parser_t<std::string>{c, inp};
-		};
-	}
+            const auto l = x.size();
+            return parser_t<std::string>{
+                    inp.substr(0, pos + l),
+                    inp.substr(pos + l)
+            };
+        };
+    }
 
 
-	template<typename P>
-	decltype(auto) some(P p)
-	{
-		return seq(p, many(p));
-	}
+    template<typename P>
+    decltype(auto) many(P p)
+    {
+        return [=](std::string inp) {
+            std::string c{};
+
+            while (!inp.empty())
+            {
+                auto a = parse(p, inp);
+                if (a.is_empty())
+                    break;
+
+                c += a.get();
+                inp = a.remain;
+            }
+
+            return parser_t<std::string>{c, inp};
+        };
+    }
 
 
-	parser_t<std::string> ident(std::string inp)
-	{
-		return parse(seq(lower, many(alphanum)), inp);
-	}
+    template<typename P>
+    decltype(auto) some(P p)
+    {
+        return seq(p, many(p));
+    }
 
 
-	decltype(auto) space(std::string inp)
-	{
-		return parse(many(sat([](char c) { return isspace(c); })), inp);
-	}
+    parser_t<std::string> ident(std::string inp)
+    {
+        return parse(seq(lower, many(alphanum)), inp);
+    }
 
 
-	template<typename P>
-	decltype(auto) token(P p)
-	{
-		return [=](std::string inp) {
-			using p_t = decltype(parse(p, inp));
-			using value_t = typename p_t::value_t;
+    decltype(auto) space(std::string inp)
+    {
+        return parse(many(sat([](char c) { return isspace(c); })), inp);
+    }
 
-			value_t value;
-			const auto r = parse(
-					seq(
-							space,
-							fmap([&value](auto v) {
-								value = v;
-								return v;
-							}, p),
-							space
-					), inp
-			);
 
-			if (r.is_empty())
-				return empty<value_t>();
-			else
-				return parser_t<value_t>{value, r.remain};
-		};
-	}
+    template<typename P>
+    decltype(auto) token(P p)
+    {
+        return [=](std::string inp) {
+            using p_t = decltype(parse(p, inp));
+            using value_t = typename p_t::value_t;
 
-	decltype(auto) nat(std::string inp)
-	{
-		const auto r = parse(some(digit), inp);
-		if (r.is_empty())
-			return empty<long>();
+            value_t value;
+            const auto r = parse(
+                    seq(
+                            space,
+                            fmap([&value](auto v) {
+                                value = v;
+                                return v;
+                            }, p),
+                            space
+                    ), inp
+            );
 
-		return parser_t<long>{std::stol(r.get()), r.remain};
-	}
+            if (r.is_empty())
+                return empty<value_t>();
+            else
+                return parser_t<value_t>{value, r.remain};
+        };
+    }
 
-	decltype(auto) intg(std::string inp)
-	{
-		return parse(pipe(
-				nat,
-				fmap(
-						[](std::string x) { return std::stol(x); },
-						seq(char_eq('-'), nat)
-				)
-		), inp);
-	}
+    decltype(auto) nat(std::string inp)
+    {
+        const auto r = parse(some(digit), inp);
+        if (r.is_empty())
+            return empty<long>();
 
-	decltype(auto) integer(std::string inp)
-	{
-		return parse(token(intg), inp);
-	}
+        return parser_t<long>{std::stol(r.get()), r.remain};
+    }
 
-	decltype(auto) natural(std::string inp)
-	{
-		return parse(token(nat), inp);
-	}
+    decltype(auto) intg(std::string inp)
+    {
+        return parse(pipe(
+                nat,
+                fmap(
+                        [](std::string x) { return std::stol(x); },
+                        seq(char_eq('-'), nat)
+                )
+        ), inp);
+    }
 
-	decltype(auto) string(std::string inp)
-	{
-		return parse(token(seq(letter, many(alphanum))), inp);
-	}
+    decltype(auto) integer(std::string inp)
+    {
+        return parse(token(intg), inp);
+    }
 
-	decltype(auto) symbol(std::string x)
-	{
-		return [=](std::string inp) {
-			return parse(token(string_eq(x)), inp);
-		};
-	}
+    decltype(auto) natural(std::string inp)
+    {
+        return parse(token(nat), inp);
+    }
+
+    decltype(auto) string(std::string inp)
+    {
+        return parse(token(seq(letter, many(alphanum))), inp);
+    }
+
+    decltype(auto) symbol(std::string x)
+    {
+        return [=](std::string inp) {
+            return parse(token(string_eq(x)), inp);
+        };
+    }
 
 }
 
@@ -349,37 +349,37 @@ namespace lparser
 template <typename T>
 std::ostream& operator<<(std::ostream& out, const lparser::parser_t<T>& p)
 {
-	if (p.is_empty())
-		out << "()";
-	else
-		out << "(" << p.get() << " ; " << p.remain << ")";
-	return out;
+    if (p.is_empty())
+        out << "()";
+    else
+        out << "(" << p.get() << " ; " << p.remain << ")";
+    return out;
 }
 
 template <typename T>
 std::ostream& operator<<(std::ostream& out, const lparser::parser_t<std::vector<T>>& v)
 {
-	out << "([";
+    out << "([";
 
-	bool f{false};
-	for (const auto& x : v.get())
-	{
-		if (f)
-			out << ",";
-		else
-			f = true;
-		out << x;
-	}
+    bool f{false};
+    for (const auto& x : v.get())
+    {
+        if (f)
+            out << ",";
+        else
+            f = true;
+        out << x;
+    }
 
-	out << "] ; " << v.remain << ")";
-	return out;
+    out << "] ; " << v.remain << ")";
+    return out;
 }
 
 template <typename T, typename R>
 std::ostream& operator<<(std::ostream& out, const std::pair<T, R>& p)
 {
-	out << "(" << p.first << ", " << p.second << ")";
-	return out;
+    out << "(" << p.first << ", " << p.second << ")";
+    return out;
 }
 
 
--- main.cpp
+++ main.cpp
@@ -21,150 +21,150 @@ parser_t<long> expr(std::string inp);
 
 parser_t<long> factor(std::string inp)
 {
-	long a{};
+    long a{};
 
-	const auto r = parse(
-			pipe(
-					seq(
-							space,
-							char_eq('('),
-							space,
-							fmap([&](long s){a = s; return std::string{};}, expr),
-							space,
-							char_eq(')'),
-							space
-					),
-					fmap([&](long s){a = s; return std::string{};}, nat)
-			),
-			inp
-	);
+    const auto r = parse(
+            pipe(
+                seq(
+                    space,
+                    char_eq('('),
+                    space,
+                    fmap([&](long s){a = s; return std::string{};}, expr),
+                    space,
+                    char_eq(')'),
+                    space
+                ),
+                fmap([&](long s){a = s; return std::string{};}, nat)
+            ),
+            inp
+    );
 
-	if (r.is_empty())
-		return empty<long>();
-	else
-		return parser_t<long>(a, r.remain);
+    if (r.is_empty())
+        return empty<long>();
+    else
+        return parser_t<long>(a, r.remain);
 }
 
 parser_t<long> term(std::string inp)
 {
-	char op;
-	long a{}, b{1};
+    char op;
+    long a{}, b{1};
 
-	const auto r = parse(
-			pipe(
-					seq(
-							space,
-							fmap([&](long s){a = s; return std::string{};}, factor),
-							space,
-							fmap([&](char c){op = c; return std::string{};}, pipe(char_eq('*'), char_eq('/'))),
-							space,
-							fmap([&](long s){b = s; return std::string{};}, term),
-							space
-					),
-					fmap([&](long s){a = s; return std::string{};}, factor)
-			),
-			inp
-	);
+    const auto r = parse(
+            pipe(
+                    seq(
+                            space,
+                            fmap([&](long s){a = s; return std::string{};}, factor),
+                            space,
+                            fmap([&](char c){op = c; return std::string{};}, pipe(char_eq('*'), char_eq('/'))),
+                            space,
+                            fmap([&](long s){b = s; return std::string{};}, term),
+                            space
+                    ),
+                    fmap([&](long s){a = s; return std::string{};}, factor)
+            ),
+            inp
+    );
 
-	if (r.is_empty())
-		return empty<long>();
-	else
-		return parser_t<long>((op == '*' ? a * b : a / b), r.remain);
+    if (r.is_empty())
+        return empty<long>();
+    else
+        return parser_t<long>((op == '*' ? a * b : a / b), r.remain);
 }
 
 parser_t<long> expr(std::string inp)
 {
-	char op;
-	long a{}, b{};
+    char op;
+    long a{}, b{};
 
-	const auto r = parse(
-			pipe(
-					seq(
-							space,
-							fmap([&](long s){a = s; return std::string{};}, term),
-							space,
-							fmap([&](char c){op = c; return std::string{};}, pipe(char_eq('+'), char_eq('-'))),
-							space,
-							fmap([&](long s){b = s; return std::string{};}, expr),
-							space
-					),
-					fmap([&](long s_a){a = s_a; return std::string{};}, term)
-			),
-			inp
-	);
+    const auto r = parse(
+            pipe(
+                    seq(
+                            space,
+                            fmap([&](long s){a = s; return std::string{};}, term),
+                            space,
+                            fmap([&](char c){op = c; return std::string{};}, pipe(char_eq('+'), char_eq('-'))),
+                            space,
+                            fmap([&](long s){b = s; return std::string{};}, expr),
+                            space
+                    ),
+                    fmap([&](long s_a){a = s_a; return std::string{};}, term)
+            ),
+            inp
+    );
 
-	if (r.is_empty())
-		return empty<long>();
-	else
-		return parser_t<long>((op == '+' ? a + b : a - b), r.remain);
+    if (r.is_empty())
+        return empty<long>();
+    else
+        return parser_t<long>((op == '+' ? a + b : a - b), r.remain);
 }
 
 
 
 int main()
 {
-	std::cout << parse(item, "") << std::endl;
-	std::cout << parse(item, "3+5") << std::endl;
-	std::cout << parse(item, "abc") << std::endl;
+    std::cout << parse(item, "") << std::endl;
+    std::cout << parse(item, "3+5") << std::endl;
+    std::cout << parse(item, "abc") << std::endl;
 
-	std::cout << parse(fmap([](char c){ return (char) ::toupper(c); }, item), "abc") << std::endl;
+    std::cout << parse(fmap([](char c){ return (char) ::toupper(c); }, item), "abc") << std::endl;
 
-	std::cout << parse(pure(1), "abc") << std::endl;
+    std::cout << parse(pure(1), "abc") << std::endl;
 
-	auto f_g = [=](char x) {
-		return [=](char y){
-			return [=](char z){
-				return std::make_pair(x, z);
-			};
-		};
-	};
+    auto f_g = [=](char x) {
+        return [=](char y){
+            return [=](char z){
+                return std::make_pair(x, z);
+            };
+        };
+    };
 
-	std::cout << parse(combine(pure(f_g), item, item, item), "abcdef") << std::endl;
-	std::cout << parse(combine(pure(f_g), item, item, item), "ab") << std::endl; //empty
+    std::cout << parse(combine(pure(f_g), item, item, item), "abcdef") << std::endl;
+    std::cout << parse(combine(pure(f_g), item, item, item), "ab") << std::endl; //empty
 
-	std::cout << parse(pipe(empty_fn<char>, item), "abc") << std::endl;
+    std::cout << parse(pipe(empty_fn<char>, item), "abc") << std::endl;
 
-	std::cout << "isDigit " << parse(digit, "123") << std::endl;
-	std::cout << "isLower " << parse(lower, "home") << std::endl;
-	std::cout << "isUpper " << parse(upper, "Home") << std::endl;
-	std::cout << "chareq " << parse(char_eq('I'), "Inghe") << std::endl;
+    std::cout << "isDigit " << parse(digit, "123") << std::endl;
+    std::cout << "isLower " << parse(lower, "home") << std::endl;
+    std::cout << "isUpper " << parse(upper, "Home") << std::endl;
+    std::cout << "chareq " << parse(char_eq('I'), "Inghe") << std::endl;
 
-	std::cout << "seq " << parse(seq(char_eq('I'), char_eq('n'), char_eq('g')), "Inghe") << std::endl;
-	std::cout << "string_eq " << parse(string_eq("Ing"), "Inghe") << std::endl;
+    std::cout << "seq " << parse(seq(char_eq('I'), char_eq('n'), char_eq('g')), "Inghe") << std::endl;
+    std::cout << "string_eq " << parse(string_eq("Ing"), "Inghe") << std::endl;
 
-	std::cout << "many(space) " << parse(space, " 123abc") << std::endl;
-	std::cout << "many(digit) " << parse(many(digit), "123abc") << std::endl;
-	std::cout << "some(digit) " << parse(some(digit), "123abc") << std::endl;
-	std::cout << "some(digit) " << parse(some(digit), "x23abc") << std::endl;
-	std::cout << "some(letter) " << parse(some(letter), "abc123abc") << std::endl;
+    std::cout << "many(space) " << parse(space, " 123abc") << std::endl;
+    std::cout << "many(digit) " << parse(many(digit), "123abc") << std::endl;
+    std::cout << "some(digit) " << parse(some(digit), "123abc") << std::endl;
+    std::cout << "some(digit) " << parse(some(digit), "x23abc") << std::endl;
+    std::cout << "some(letter) " << parse(some(letter), "abc123abc") << std::endl;
 
-	std::cout << "ident " << parse(ident, "abc123 abc") << std::endl;
-	std::cout << "nat " << parse(nat, "1789abc") << std::endl;
-	std::cout << "space " << parse(space, "    abc") << std::endl;
+    std::cout << "ident " << parse(ident, "abc123 abc") << std::endl;
+    std::cout << "nat " << parse(nat, "1789abc") << std::endl;
+    std::cout << "space " << parse(space, "    abc") << std::endl;
 
-	std::cout << "intg " << parse(intg, "1235") << std::endl;
-	std::cout << "intg " << parse(intg, "-1235   xxx") << std::endl;
-	std::cout << "integer " << parse(integer, "  -107     95") << std::endl;
+    std::cout << "intg " << parse(intg, "1235") << std::endl;
+    std::cout << "intg " << parse(intg, "-1235   xxx") << std::endl;
+    std::cout << "integer " << parse(integer, "  -107     95") << std::endl;
 
-	std::cout << "symbol " << parse(symbol("A"), " [ x ] ") << std::endl;
-	std::cout << "symbol " << parse(symbol("x"), " x = 123 ") << std::endl;
+    std::cout << "symbol " << parse(symbol("A"), " [ x ] ") << std::endl;
+    std::cout << "symbol " << parse(symbol("x"), " x = 123 ") << std::endl;
 
-	std::cout << "assign " << parse(assign, "abc = x123") << std::endl;
+    std::cout << "assign " << parse(assign, "abc = x123") << std::endl;
 
-	std::cout << "nats " << parse(nats, "[5, 8, 1982, 3 ]") << std::endl;
+    std::cout << "nats " << parse(nats, "[5, 8, 1982, 3 ]") << std::endl;
 
-	const std::string expr_s = "((5  * 7) + (31 - 9) + 21);finish!";
-	const auto b_expr = parse(expr, expr_s);
-	if (b_expr.is_empty())
-	{
-		std::cout << "invalid expr: " << expr_s << std::endl;
-	}
-	else
-	{
-		std::cout << "expr: " << expr_s << " = "
-				  << *b_expr.first
-				  << ", not parsed: " << b_expr.remain
-				  << std::endl;
-	}
-	return 0;
+    const std::string expr_s = "((5  * 7) + (31 - 9) + 21);finish!";
+    const auto b_expr = parse(expr, expr_s);
+    if (b_expr.is_empty())
+    {
+        std::cout << "invalid expr: " << expr_s << std::endl;
+    }
+    else
+    {
+        std::cout << "expr: " << expr_s << " = "
+                  << *b_expr.first
+                  << ", not parsed: " << b_expr.remain
+                  << std::endl;
+    }
+    return 0;
 }
